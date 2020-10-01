c95ddba1daac5c4259b3c4c557cec529fccf82bc
Tony Vu <52386267+tonyvu1@users.noreply.github.com>
2020-10-01 17:14:14 -0500

    Fixed https service

--- client/src/app/https.service.ts
+++ client/src/app/https.service.ts
@@ -12,7 +12,7 @@ export class HttpsService {
       var currentUrl = window.location.href;
       var newUrl = "https://";
       if (currentUrl.charAt(4) == ":") {
-        currentUrl = currentUrl.substring(7, currentUrl.length - 1);
+        currentUrl = currentUrl.substring(7, currentUrl.length);
         newUrl += currentUrl;
         window.location.href = newUrl;
       }
