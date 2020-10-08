b85b94d1b13a4ccbd74aaa2ddf7fbc2f22979fbd
Tony Vu <52386267+tonyvu1@users.noreply.github.com>
2020-10-08 17:01:21 -0500

    Removed unused dependencies and scripts

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,19 +3,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
-import { MatButtonModule } from "@angular/material/button";
-import { MatSidenavModule } from "@angular/material/sidenav";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HomeComponent } from "./portfolio/home/home.component";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatIconModule } from "@angular/material/icon";
-import { MatListModule } from "@angular/material/list";
 import { ProjectsComponent } from "./portfolio/projects/projects.component";
 import { ContactComponent } from "./portfolio/contact/contact.component";
 import { ResumeComponent } from "./portfolio/resume/resume.component";
-import { ParticlesModule } from "angular-particle";
 
 // POS
 import { ToastrModule } from "ngx-toastr";
@@ -72,17 +66,12 @@ import { NavComponent } from './portfolio/nav/nav.component';
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatListModule,
     HttpClientModule,
     ToastrModule.forRoot({
       timeOut: 3000,
       preventDuplicates: true,
     }),
-    ParticlesModule,
+
   ],
   providers: [PosGlobals, RegisterService],
   bootstrap: [AppComponent],
